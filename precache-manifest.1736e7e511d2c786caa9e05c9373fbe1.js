df1c07d7728f5a65ee735e225eb28b02ea56c7dd
Zhiqi Lin <zkzhiqilin@gmail.com>
2020-11-18 12:50:15 -0800

    Updates

---
+++ precache-manifest.1736e7e511d2c786caa9e05c9373fbe1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0228625ead1e535c5992b832fd1a5219",
+    "url": "/ghibli-poster-color-rough/index.html"
+  },
+  {
+    "revision": "b29495beaa3140debb38",
+    "url": "/ghibli-poster-color-rough/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "8f565c5f3afbd6108947",
+    "url": "/ghibli-poster-color-rough/static/css/main.e3a14eb1.chunk.css"
+  },
+  {
+    "revision": "b29495beaa3140debb38",
+    "url": "/ghibli-poster-color-rough/static/js/2.aaa90e16.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ghibli-poster-color-rough/static/js/2.aaa90e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f565c5f3afbd6108947",
+    "url": "/ghibli-poster-color-rough/static/js/main.7d759f9d.chunk.js"
+  },
+  {
+    "revision": "8364941c14527a196e2a",
+    "url": "/ghibli-poster-color-rough/static/js/runtime-main.e1b49daa.js"
+  }
+]);
